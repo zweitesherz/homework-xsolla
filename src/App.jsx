cfdad9577f03a77c80f361007427761996ed2823
Maria <zweitesherz@mail.ru>
2020-08-10 19:33:59 +0500

    config standart

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,20 @@
 
-import { hot } from 'react-hot-loader/root';
+import { hot } from 'react-hot-loader/root'
 
-import React from 'react';
+import React from 'react'
 
-import logoSrc from './logo.png';
+import logoSrc from './logo.png'
 
-import stLess from './style.less';
+import stLess from './style.less'
 
-function App() {
-
-    return <div className={stLess.message}>
-        <img className={stLess.logo} alt="Xsolla2" src={logoSrc}/>
+function App () {
+  return <div className={stLess.message}>
+    <img className={stLess.logo} alt="Xsolla2" src={logoSrc}/>
         Hello Xsolla school, hello!
-        <br/>
+    <br/>
 
-        <input/>
-    </div>
+    <input/>
+  </div>
 }
 
-export default hot(App);
-
+export default hot(App)
